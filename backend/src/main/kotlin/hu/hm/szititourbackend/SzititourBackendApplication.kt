42d42b9c669dab6f8842eac88ee783dca82f7d7e
Herczku Máté <mherczku@gmail.com>
2022-10-14 11:31:30 +0200

    Add cors configuration

--- backend/src/main/kotlin/hu/hm/szititourbackend/SzititourBackendApplication.kt
+++ backend/src/main/kotlin/hu/hm/szititourbackend/SzititourBackendApplication.kt
@@ -2,10 +2,37 @@ package hu.hm.szititourbackend
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import org.springframework.web.filter.CorsFilter
+
 
 @SpringBootApplication
-class SzititourBackendApplication
+class SzititourBackendApplication {
+    @Bean
+    fun corsFilter(): CorsFilter {
+        val corsConfiguration = CorsConfiguration()
+
+        corsConfiguration.allowCredentials = true
+        corsConfiguration.allowedOrigins = listOf("http://localhost:4200")
+        corsConfiguration.allowedHeaders = listOf(
+            "Origin", "Access-Control-Allow-Origin", "Content-Type",
+            "Accept", "Authorization", "Origin, Accept", "X-Requested-With",
+            "Access-Control-Request-Method", "Access-Control-Request-Headers"
+        )
+        corsConfiguration.exposedHeaders = listOf(
+            "Origin", "Content-Type", "Accept", "Authorization",
+            "Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"
+        )
+        corsConfiguration.allowedMethods = listOf("GET", "POST", "PUT", "DELETE", "OPTIONS")
+
+        val urlBasedCorsConfigurationSource = UrlBasedCorsConfigurationSource()
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration)
+        return CorsFilter(urlBasedCorsConfigurationSource)
+    }
+}
 
 fun main(args: Array<String>) {
-	runApplication<SzititourBackendApplication>(*args)
+    runApplication<SzititourBackendApplication>(*args)
 }
