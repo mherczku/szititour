772dfbbd78be1fbfd45f3f22798b0dc12d854128
Herczku Máté <mherczku@gmail.com>
2022-11-13 15:58:03 +0100

    Implement Login

--- frontend/src/app/pages/login/login.component.ts
+++ frontend/src/app/pages/login/login.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {AuthService} from "../../services/AuthService";
 import {UserService} from "../../services/UserService";
 import {ButtonType} from "../../enums/button-type";
+import {NetworkResponse} from "../../interfaces/network-response";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -9,17 +11,31 @@ import {ButtonType} from "../../enums/button-type";
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+
   ButtonType = ButtonType;
 
-  constructor(private authService: AuthService, private userService: UserService) {
+  email: string = ""
+  password: string = ""
+  error: string = ""
+
+  constructor(private authService: AuthService, private userService: UserService, private router: Router) {
   }
 
   ngOnInit(): void {
   }
 
   login() {
-    this.authService.login("t@test.hu", "1234").subscribe(res => {
+    if(this.email === 't') {
+      this.email = "t@test.hu"
+      this.password = "1234"
+    }
+    this.authService.login(this.email, this.password).subscribe((res: NetworkResponse) => {
       console.log(res, "res")
+      if(res.success){
+        this.router.navigateByUrl("/admin")
+      } else {
+        this.error = res.errorMessage
+      }
     })
 
   }
