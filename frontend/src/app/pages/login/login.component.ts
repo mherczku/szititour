a33f845fa18cce75f29807f375312180434c83e7
Herczku Máté <mherczku@gmail.com>
2022-11-14 18:53:04 +0100

    Implement login with toasts

--- frontend/src/app/pages/login/login.component.ts
+++ frontend/src/app/pages/login/login.component.ts
@@ -4,6 +4,8 @@ import {UserService} from "../../services/UserService";
 import {ButtonType} from "../../enums/button-type";
 import {NetworkResponse} from "../../interfaces/network-response";
 import {Router} from "@angular/router";
+import {HotToastService} from "@ngneat/hot-toast";
+import {HttpErrorResponse} from "@angular/common/http";
 
 @Component({
   selector: 'app-login',
@@ -18,20 +20,33 @@ export class LoginComponent implements OnInit {
   password: string = ""
   error: string = ""
 
-  constructor(private authService: AuthService, private userService: UserService, private router: Router) {
+  constructor(private authService: AuthService, private userService: UserService, private router: Router, private toastService: HotToastService) {
   }
 
   ngOnInit(): void {
   }
 
   login() {
-    if(this.email === 't') {
+    if (this.email === 't') {
       this.email = "t@test.hu"
       this.password = "1234"
     }
-    this.authService.login(this.email, this.password).subscribe((res: NetworkResponse) => {
-      console.log(res, "res")
-      if(res.success){
+    this.authService.login(this.email, this.password)
+      .pipe(
+        this.toastService.observe(
+          {
+            loading: 'Logging in...',
+            success: (s) => {
+              if(s.success){
+                return 'Logged in'
+              }
+              return 'Login failed'
+            },
+            error: (e: HttpErrorResponse) => 'Something went wrong',
+          }
+        )
+      ).subscribe((res: NetworkResponse) => {
+      if (res.success) {
         this.router.navigateByUrl("/admin")
       } else {
         this.error = res.errorMessage
