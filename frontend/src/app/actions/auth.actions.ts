f3c1f292311e20681734b4598ef1e1b13b2257b7
Herczku Máté <mherczku@gmail.com>
2022-11-24 20:15:02 +0100

    Implement auth actions

---
+++ frontend/src/app/actions/auth.actions.ts
@@ -0,0 +1,11 @@
+import { createAction, props } from '@ngrx/store'
+import {Team} from "../interfaces/team";
+
+export const login = createAction(
+    '[Auth] Login',
+    props<{ team: Team }>()
+)
+
+export const logout = createAction(
+    '[Auth] Logout'
+)
