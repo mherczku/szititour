740c7c1cc399ba206e58d4e42968ca627ddd359f
Herczku Máté <mherczku@gmail.com>
2022-11-28 20:56:15 +0100

    Implement missing admin service function

--- frontend/src/app/services/AdminService.ts
+++ frontend/src/app/services/AdminService.ts
@@ -1,7 +1,11 @@
 import {Injectable} from "@angular/core";
 import {environment} from "../../environments/environment";
 import {HttpClient} from "@angular/common/http";
-import {Observable} from "rxjs";
+import {Observable, tap} from "rxjs";
+import {Game} from "../interfaces/game";
+import {Application} from "../interfaces/application";
+import {ApplicationOriginal} from "../interfaces/application-original";
+import {Place} from "../interfaces/place";
 
 
 @Injectable({providedIn: 'root'})
@@ -15,4 +19,29 @@ export class AdminService {
     return this.http.get<unknown>(`${this.baseUrl}/games`)
   }
 
+  createGame(newGame: Game): Observable<Game> {
+    return this.http.post<Game>(`${this.baseUrl}/games`, newGame)
+  }
+
+  updateGame(game: Game): Observable<Game> {
+    return this.http.put<Game>(`${this.baseUrl}/games`, game)
+  }
+
+  deleteGame(id: number): Observable<unknown> {
+    return this.http.delete<unknown>(`${this.baseUrl}/games/${id}`)
+  }
+
+  reviewApplication(application: Application, isAccepted: boolean): Observable<Application> {
+    const updated: ApplicationOriginal = {
+      id: application.id,
+      accepted: isAccepted,
+      game: {id: application.gameId},
+      team: {id: application.teamId}
+    }
+    return this.http.put<Application>(`${this.baseUrl}/applications/`, updated)
+  }
+
+  getPlaceById(placeId: number): Observable<Place> {
+    return this.http.get<Place>(`${this.baseUrl}/places/${placeId}`)
+  }
 }
