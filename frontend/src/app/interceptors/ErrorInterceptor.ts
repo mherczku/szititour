8f0eaa75a58bbccca7e46cc81ba44241bac237b8
Herczku Máté <mherczku@gmail.com>
2022-11-28 20:53:18 +0100

    add more cases to ErrorInterceptor.ts

--- frontend/src/app/interceptors/ErrorInterceptor.ts
+++ frontend/src/app/interceptors/ErrorInterceptor.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {
   HttpRequest,
   HttpHandler,
@@ -7,30 +7,48 @@ import {
   HttpErrorResponse,
   HttpStatusCode
 } from '@angular/common/http';
-import { catchError, Observable, throwError } from 'rxjs';
+import {catchError, Observable, throwError} from 'rxjs';
 import {HotToastService} from "@ngneat/hot-toast";
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
-  constructor(private toastService: HotToastService) {}
+  constructor(private toastService: HotToastService) {
+  }
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     return next.handle(request).pipe(
       catchError((err: any, caught: any) => {
-        if(err instanceof HttpErrorResponse) {
-          switch (err.status) {
-            case HttpStatusCode.Unauthorized: {
-              this.toastService.error("Unauthorized request")
-              break;
-            }
-            case HttpStatusCode.BadRequest: {
-              this.toastService.error("Bad Request")
-              break;
-            }
-            case HttpStatusCode.InternalServerError: {
-              this.toastService.error("Internal Error")
-              break;
+        if (err instanceof HttpErrorResponse) {
+          if (err?.error?.errorMessage) {
+            this.toastService.error(err.error.errorMessage)
+          }
+          else {
+            switch (err.status) {
+              case HttpStatusCode.GatewayTimeout: {
+                this.toastService.error("Gateway Timed Out")
+                break;
+              }
+              case HttpStatusCode.RequestTimeout: {
+                this.toastService.error("Request Timed Out")
+                break;
+              }
+              case HttpStatusCode.Unauthorized: {
+                this.toastService.error("Unauthorized request")
+                break;
+              }
+              case HttpStatusCode.BadRequest: {
+                this.toastService.error("Bad Request")
+                break;
+              }
+              case HttpStatusCode.InternalServerError: {
+                this.toastService.error("Internal Error")
+                break;
+              }
+              case HttpStatusCode.NotFound: {
+                this.toastService.error("Requested object not found")
+                break;
+              }
             }
           }
         }
