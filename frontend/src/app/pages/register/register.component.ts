336404df2fb75b493531d3fa5e7c84b764fc9896
Herczku Máté <mherczku@gmail.com>
2022-11-28 20:49:33 +0100

    Implement register logic

--- frontend/src/app/pages/register/register.component.ts
+++ frontend/src/app/pages/register/register.component.ts
@@ -1,17 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ButtonType} from "../../enums/button-type";
+import {HotToastService} from "@ngneat/hot-toast";
+import {AuthService} from "../../services/AuthService";
+import {Subscription} from "rxjs";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent implements OnDestroy {
+
   ButtonType = ButtonType;
 
-  constructor() { }
+  email: string = "";
+  password: string = "";
+  passwordConfirm: string = "";
+  registering: boolean = false;
 
-  ngOnInit(): void {
+  subscriptionRegister? : Subscription
+
+  constructor(private alertService: HotToastService, private authService: AuthService, private router: Router) { }
+
+  // todo validators
+  register() {
+    if(this.email !== "" && this.password !== "") {
+      if(this.password === this.passwordConfirm) {
+        this.subscriptionRegister = this.authService.register(this.email, this.password).subscribe(res => {
+          if(res.success) {
+            this.alertService.success("Successful registration, you can login now.")
+            this.router.navigateByUrl("/login")
+          }
+          else {
+            this.alertService.error("Something went wrong: " + res.errorMessage)
+          }
+        })
+      }
+      else {
+        this.alertService.warning("Password and Password Confirm are different")
+      }
+    }
+    else {
+      this.alertService.warning("Fill in all fields")
+    }
   }
 
+  ngOnDestroy(): void {
+    this.subscriptionRegister?.unsubscribe()
+  }
 }
