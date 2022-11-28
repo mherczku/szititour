96eca4954bd79f28b9c3e6557e163b77eb9d53db
Herczku Máté <mherczku@gmail.com>
2022-11-28 20:50:24 +0100

    Fix gamecard and delete

--- frontend/src/app/components/cards/gamecard/gamecard.component.ts
+++ frontend/src/app/components/cards/gamecard/gamecard.component.ts
@@ -1,6 +1,9 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnDestroy, OnInit, Output} from '@angular/core';
 import { ButtonType } from 'src/app/enums/button-type';
 import {Game} from "../../../interfaces/game";
+import {HotToastService} from "@ngneat/hot-toast";
+import {AdminService} from "../../../services/AdminService";
+import {Subscription} from "rxjs";
 
 
 @Component({
@@ -8,18 +11,39 @@ import {Game} from "../../../interfaces/game";
   templateUrl: './gamecard.component.html',
   styleUrls: ['./gamecard.component.css']
 })
-export class GamecardComponent implements OnInit {
+export class GamecardComponent implements OnInit, OnDestroy {
 
-  @Input() game: Game = {applications: ["t1", "t2"], dateEnd: "2020.10.10", dateStart: "2020.10.10", id: 0, img: "assets/img/sample.jpg", places: [], title: "SampleTitle"}
+  @Input() game: Game = {applications: [], id: 0, img: "assets/img/sample.jpg", places: [], title: "SampleTitle", dateStart: new Date(), dateEnd: new Date()}
   @Output() onEditClicked: EventEmitter<unknown> = new EventEmitter<unknown>()
   @Output() onTeamsClicked: EventEmitter<unknown> = new EventEmitter<unknown>()
   @Output() onPlacesClicked: EventEmitter<unknown> = new EventEmitter<unknown>()
+  @Output() onDeleted: EventEmitter<unknown> = new EventEmitter<unknown>()
 
   ButtonType = ButtonType;
 
-  constructor() {}
+  deleting: boolean = false;
+  subscriptionDelete?: Subscription;
+
+  constructor(private alert: HotToastService, private adminService: AdminService) {}
 
   ngOnInit(): void {
+  }
+
+  deleteGame() {
+    const sure = window.confirm(`Are you sure to delete ${this.game.title}?`)
+    if(sure){
+      this.deleting = true
+      this.subscriptionDelete = this.adminService.deleteGame(this.game.id).subscribe({
+        next: res => {
+          this.deleting = false
+          this.alert.success(`${this.game.title} successfully deleted`)
+          this.onDeleted.emit()
+        },
+        error: err => {
+          this.deleting = false
+        }
+      })
+    }
   }
 
   getAcceptedApplicationsLength() {
@@ -27,4 +51,7 @@ export class GamecardComponent implements OnInit {
     //this.game.applications.filter(e => e.)
   }
 
+  ngOnDestroy(): void {
+    this.subscriptionDelete?.unsubscribe()
+  }
 }
