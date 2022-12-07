eb6516d5dee166b77d90aed1763d9eb67f68254d
Herczku Máté <mherczku@gmail.com>
2022-12-07 19:33:45 +0100

    implement imgSrc pipe

---
+++ frontend/src/app/pipes/img-src/img-src.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {ImgSrcPipe} from "./img-src.pipe";
+
+
+
+@NgModule({
+  declarations: [ImgSrcPipe],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    ImgSrcPipe
+  ]
+})
+export class ImgSrcModule { }
---
+++ frontend/src/app/pipes/img-src/img-src.pipe.ts
@@ -0,0 +1,14 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import {environment} from "../../../environments/environment";
+
+@Pipe({
+  name: 'imgSrc'
+})
+export class ImgSrcPipe implements PipeTransform {
+
+  transform(value?: string): string {
+    const token = localStorage.getItem('auth-token')
+    return environment.apiBaseUrl + '/resources/images?Authorization=' + token + '&image=' + value
+  }
+
+}
