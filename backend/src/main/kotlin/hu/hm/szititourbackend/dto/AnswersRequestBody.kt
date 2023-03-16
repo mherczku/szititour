ce1fbeea4cffe4f2030f04bf7682f59f30925e57
Herczku Máté <mherczku@gmail.com>
2023-03-16 12:31:09 +0100

    implement answersRequestBody

---
+++ backend/src/main/kotlin/hu/hm/szititourbackend/dto/AnswersRequestBody.kt
@@ -0,0 +1,13 @@
+package hu.hm.szititourbackend.dto
+
+import hu.hm.szititourbackend.datamodel.Answer
+
+class AnswersRequestBody (
+    val gameId: Int = 0,
+    val questionAnswers: List<QuestionAnswer> = listOf()
+)
+
+class QuestionAnswer(
+    var questionId: Int = 0,
+    var answer: Answer = Answer()
+)
