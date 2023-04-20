01778e1ca151a40c874da48079e93227ec608b1f
Herczku Máté <mherczku@gmail.com>
2023-04-20 22:49:20 +0200

    implement evaluate answers logic frontend

--- frontend/src/app/ui/components/admin/answer/answer.component.ts
+++ frontend/src/app/ui/components/admin/answer/answer.component.ts
@@ -3,6 +3,9 @@ import {CommonModule} from "@angular/common";
 import {AnswerDto} from "../../../../types/team-game-status";
 import {QuestionType} from "../../../../enums/question-type";
 import {ImgSrcModule} from "../../../../pipes/img-src/img-src.module";
+import {AdminActiveGameService} from "../../../../services/AdminActiveGameService";
+import {AutoDestroy} from "../../../../decorators/autodestroy.decorator";
+import {Subject, takeUntil} from "rxjs";
 
 @Component({
   selector: "app-answer",
@@ -35,7 +38,14 @@ export class AnswerComponent {
   protected readonly QuestionType = QuestionType;
   evaluateId?: number;
 
-  evaluateAnswer(isCorrect: boolean) {
+  @AutoDestroy destroy$ = new Subject();
 
+  constructor(private adminActiveService: AdminActiveGameService) {
+  }
+
+  evaluateAnswer(isCorrect: boolean) {
+    this.adminActiveService.evaluateAnswer(this.answer.id, isCorrect).pipe(takeUntil(this.destroy$)).subscribe(answer => {
+      this.answer.correct = answer.correct;
+    });
   }
 }
