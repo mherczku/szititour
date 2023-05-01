591533d9a46f2217d056fd92a802be60bcb1bcad
Herczku Máté <mherczku@gmail.com>
2023-05-01 20:42:36 +0200

    map load only once

--- frontend/src/app/e-functions/extension-functions.ts
+++ frontend/src/app/e-functions/extension-functions.ts
@@ -1,3 +1,20 @@
+import {environment} from "../../environments/environment";
+
 export function myTrackBy(index: number, item: any): number {
     return item.id ?? index;
+}
+
+export function addMapApiHeader(onLoad: () => void) {
+  if (!document.getElementById("map-script")) {
+    const script = document.createElement("script");
+    script.id = "map-script";
+    script.type = "text/javascript";
+    script.src = `https://maps.googleapis.com/maps/api/js?key=${environment.MAP_KEY}&callback=gmNoop`;
+    script.onload = () => {
+      onLoad();
+    };
+    document.head.appendChild(script);
+  } else {
+    onLoad();
+  }
 }
