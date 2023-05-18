1e74a264bd19be99b9d2f4d5ca49d18d58213825
Herczku Máté <mherczku@gmail.com>
2023-05-18 12:20:54 +0200

    fix home welcome

--- frontend/src/app/ui/pages/user/home/home.component.ts
+++ frontend/src/app/ui/pages/user/home/home.component.ts
@@ -4,6 +4,8 @@ import {UserGameCardComponent} from "../../../components/user/cards/user-game-ca
 import {UserService} from "../../../../services/UserService";
 import {Observable} from "rxjs";
 import {Game} from "../../../../types/game";
+import {Store} from "@ngrx/store";
+import {selectLoggedInTeam} from "../../../../store/selectors/auth.selector";
 
 @Component({
   selector: "app-home",
@@ -16,8 +18,9 @@ import {Game} from "../../../../types/game";
 export class HomeComponent implements OnInit {
   /*@AutoDestroy destroy = new Subject();*/
   games!: Observable<Game[]>;
+  profile = this.store.select(selectLoggedInTeam);
 
-  constructor(private userService: UserService) {
+  constructor(private store: Store, private userService: UserService) {
   }
 
 
