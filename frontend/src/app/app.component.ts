f99f199156573045643fa347b0d745c5ab0faae5
Herczku Máté <mherczku@gmail.com>
2023-06-04 11:36:30 +0200

    add route animations

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from "@angular/core";
+import {ChildrenOutletContexts} from "@angular/router";
+import {routeAnimations} from "./ui/animations/route.animation";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  styleUrls: ["./app.component.css"],
+  animations: [routeAnimations]
 })
 export class AppComponent {
   title = "szititour";
+
+  constructor(private contexts: ChildrenOutletContexts) {
+  }
+  get getRouteAnimationData(): string {
+    return this.contexts.getContext("primary")?.route?.snapshot?.data?.["animation"] ?? this.contexts.getContext("primary")?.route?.snapshot?.toString();
+  }
 }
