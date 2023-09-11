d4f12a3cdc6f2b764ba1bca4535733b2c2c27990
Herczku <mherczku@gmail.com>
2023-09-11 17:53:58 +0200

    fix: env-js

--- frontend/src/assets/js/process-env.js
+++ frontend/src/assets/js/process-env.js
@@ -2,56 +2,27 @@ const fs = require("fs");
 const path = require("path");
 
 const dir = "frontend/src/environments";
-const file = "environment.ts";
 const prodFile = "environment.prod.ts";
 
-const FIREBASE_CONFIG = `${process.env.FIREBASE_CONFIG}`;
-const GOOGLE_ID = `${process.env.GOOGLE_ID}`;
-const MAP_KEY = `${process.env.MAP_KEY}`;
-const VPKEY = `${process.env.VPKEY}`;
+const content = `${process.env.ENV_STAGE}`;
 
 fs.access(dir, fs.constants.F_OK, (err) => {
-    if (err) {
+    if(err) {
         // Dir not exist
         console.log("src not exist, creating now", process.cwd());
-        fs.mkdir(dir, { recursive: true }, (err) => {
-            if (err) throw err;
+        fs.mkdir(dir, {recursive: true}, (err) => {
+            if(err) throw err;
         });
     }
 
     // Write content to file
     try {
-        fs.readFile(dir + "/" + prodFile, 'utf-8', function (err, contents) {
-            if (err) {
-                console.log(err);
-                process.exit(1);
-            }
+        fs.writeFileSync(dir + "/" + prodFile, content);
+        console.log("Write successfull in", process.cwd());
 
-            console.log("contents: " + contents);
-
-            let replaced = contents.replace("SECRET_MAP_KEY", MAP_KEY);
-            replaced = replaced.replace("SECRET_GOOGLE_ID", GOOGLE_ID);
-            replaced = replaced.replace("SECRET_FIREBASE_CONFIG", FIREBASE_CONFIG);
-            replaced = replaced.replace("SECRET_VPKEY", VPKEY);
-
-            console.log("replaced: " + replaced);
-
-            fs.writeFileSync(dir + "/" + file, replaced);
-            fs.writeFileSync(dir + "/" + prodFile, replaced + "//TEST SZÖVEG BELEMEGY E");
-            console.log("Write successfull in", process.cwd());
-
-            if (fs.existsSync(dir + "/" + file)) {
-                console.log("File created", path.resolve(dir + "/" + file));
-            }
-            if (fs.existsSync(dir + "/" + prodFile)) {
-                console.log("File created", path.resolve(dir + "/" + prodFile));
-                const str = fs.readFileSync(dir + "/" + prodFile).toString();
-                console.log(str);
-            }
-
-
-        });
-
+        if(fs.existsSync(dir + "/" + prodFile)) {
+            console.log("File created", path.resolve(dir + "/" + prodFile));
+        }
     } catch (err) {
         console.log(err);
         process.exit(1);
