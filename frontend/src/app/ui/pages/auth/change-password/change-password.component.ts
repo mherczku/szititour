7c5aa21de70ffabc6d7ba88ea53344354bd60b84
Herczku <mherczku@gmail.com>
2023-10-02 17:19:33 +0200

    feat: fe auth password component

---
+++ frontend/src/app/ui/pages/auth/change-password/change-password.component.ts
@@ -0,0 +1,16 @@
+import { ChangeDetectionStrategy, Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { PasswordChangeComponent } from "../../../components/password-change/password-change.component";
+
+@Component({
+    standalone: true,
+    templateUrl: "./change-password.component.html",
+    styleUrls: ["./change-password.component.scss"],
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    imports: [CommonModule, PasswordChangeComponent]
+})
+export class ChangePasswordComponent {
+
+
+}
+
