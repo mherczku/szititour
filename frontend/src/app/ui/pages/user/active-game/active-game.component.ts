bc6e0a0e7bc9466063805ade2f12741866a78902
Herczku <mherczku@gmail.com>
2023-10-04 20:25:33 +0200

    fix: fe active onpush user

--- frontend/src/app/ui/pages/user/active-game/active-game.component.ts
+++ frontend/src/app/ui/pages/user/active-game/active-game.component.ts
@@ -1,38 +1,44 @@
-import {Component, OnInit} from "@angular/core";
-import {CommonModule} from "@angular/common";
-import {ActivatedRoute} from "@angular/router";
-import {ActiveGameService} from "../../../../services/ActiveGameService";
-import {Observable, Subject, takeUntil, tap} from "rxjs";
-import {ActiveGame} from "../../../../types/game";
-import {PlaceCardComponent} from "../../../components/user/place-card/place-card.component";
-import {ActivePlace} from "../../../../types/place";
-import {QuestionCardComponent} from "../../../components/user/question-card/question-card.component";
-import {Store} from "@ngrx/store";
-import {selectGameStateStatuses} from "../../../../store/selectors/game-state.selector";
-import {AnswerRequest, QuestionAnswer} from "../../../../types/requests/answer-request";
-import {AutoDestroy} from "../../../../decorators/autodestroy.decorator";
-import {loadGameState} from "../../../../store/actions/game-state.actions";
-import {LocationService} from "../../../../services/LocationService";
-import {myTrackBy} from "../../../../e-functions/extension-functions";
+import { ChangeDetectionStrategy, Component, DestroyRef, OnInit, WritableSignal, signal } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { ActivatedRoute } from "@angular/router";
+import { ActiveGameService } from "../../../../services/ActiveGameService";
+import { Observable, tap } from "rxjs";
+import { ActiveGame } from "../../../../types/game";
+import { PlaceCardComponent } from "../../../components/user/place-card/place-card.component";
+import { ActivePlace } from "../../../../types/place";
+import { QuestionCardComponent } from "../../../components/user/question-card/question-card.component";
+import { Store } from "@ngrx/store";
+import { selectGameStateStatuses } from "../../../../store/selectors/game-state.selector";
+import { AnswerRequest, QuestionAnswer } from "../../../../types/requests/answer-request";
+import { loadGameState } from "../../../../store/actions/game-state.actions";
+import { LocationService } from "../../../../services/LocationService";
+import { myTrackBy } from "../../../../e-functions/extension-functions";
+import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
 
 @Component({
   selector: "app-active-game",
   standalone: true,
   imports: [CommonModule, PlaceCardComponent, QuestionCardComponent],
   templateUrl: "./active-game.component.html",
-  styleUrls: ["./active-game.component.scss"]
+  styleUrls: ["./active-game.component.scss"],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ActiveGameComponent implements OnInit {
 
-  constructor(private activatedRoute: ActivatedRoute, private activeGameService: ActiveGameService, private store: Store, private locationsService: LocationService) {
-  }
-
-  @AutoDestroy destroy$ = new Subject();
+  protected readonly myTrackBy = myTrackBy;
 
   activeGame$?: Observable<ActiveGame>;
-  selectedPlace?: ActivePlace;
-  teamStatus = this.store.select(selectGameStateStatuses);
+  $selectedPlace: WritableSignal<ActivePlace | undefined> = signal(undefined);
+  teamStatus$ = this.store.select(selectGameStateStatuses);
   gameId = -1;
+
+  constructor(
+    private readonly activatedRoute: ActivatedRoute,
+    private readonly activeGameService: ActiveGameService,
+    private readonly store: Store,
+    private readonly locationsService: LocationService,
+    private readonly destroyRef: DestroyRef) { }
+
 
   ngOnInit(): void {
     this.activatedRoute.params.subscribe(params => {
@@ -42,7 +48,7 @@ export class ActiveGameComponent implements OnInit {
         this.locationsService.trackMe(this.gameId);
         //this.activeGameService.loadTeamGameStatus(id);
         this.activeGame$ = this.activeGameService.getActiveGameData(id).pipe(tap(data => {
-          this.selectedPlace = data.places[0];
+          this.$selectedPlace.set(data.places[0]);
         }));
       }
     });
@@ -50,7 +56,7 @@ export class ActiveGameComponent implements OnInit {
 
   selectPlace(place: ActivePlace) {
     if (place.selectable) {
-      this.selectedPlace = place;
+      this.$selectedPlace.set(place);
     }
   }
 
@@ -58,16 +64,16 @@ export class ActiveGameComponent implements OnInit {
     const answers: QuestionAnswer[] = [];
     this.activeGameService.changedAnswers.forEach((value: AnswerRequest, key: number) => {
       if (value.imgFile) {
-        this.activeGameService.answerQuestionWithImage(key, value.imgFile).pipe(takeUntil(this.destroy$)).subscribe((res) => {
+        this.activeGameService.answerQuestionWithImage(key, value.imgFile).pipe(takeUntilDestroyed(this.destroyRef)).subscribe((res) => {
           this.activeGameService.changedAnswers.delete(key);
-          this.store.dispatch(loadGameState({gameState: res}));
+          this.store.dispatch(loadGameState({ gameState: res }));
         });
       } else {
-        answers.push({questionId: key, answer: value});
+        answers.push({ questionId: key, answer: value });
       }
     });
-    if(answers.length > 0){
-      this.activeGameService.answerQuestions(this.gameId, answers).pipe(takeUntil(this.destroy$)).subscribe((res) => {
+    if (answers.length > 0) {
+      this.activeGameService.answerQuestions(this.gameId, answers).pipe(takeUntilDestroyed(this.destroyRef)).subscribe(() => {
         answers.forEach((answer) => {
           this.activeGameService.changedAnswers.delete(answer.questionId);
         });
@@ -75,6 +81,4 @@ export class ActiveGameComponent implements OnInit {
     }
   }
 
-
-    protected readonly myTrackBy = myTrackBy;
 }
