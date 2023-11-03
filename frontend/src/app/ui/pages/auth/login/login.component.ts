49a6378392a4f41043a1cc9253b4c9b05bab2039
Herczku <mherczku@gmail.com>
2023-11-03 15:54:05 +0100

    fix: fe em

--- frontend/src/app/ui/pages/auth/login/login.component.ts
+++ frontend/src/app/ui/pages/auth/login/login.component.ts
@@ -41,7 +41,7 @@ export class LoginComponent implements OnInit {
 
   login() {
     if (this.email === "t" || this.email === "tu@test.hu") {
-      this.email = "tu@test.hu";
+      this.email = "t@test.hu";
       this.password = "T12345678910";
     }
     else if (this.email === "ta" || this.email === "ta@test.hu") {
